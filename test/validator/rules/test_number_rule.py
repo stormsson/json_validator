0cc57ba414764724767722549109402ba49370f7
Camillo Camarda <camillo.camarda@akqa.com>
2019-05-04 00:16:13 +0200

    updated number rule

--- test/validator/rules/test_number_rule.py
+++ test/validator/rules/test_number_rule.py
@@ -19,8 +19,18 @@ class TestNumberRule(unittest.TestCase):
         self.assertTrue(x.validate(5))
         self.assertTrue(x.validate(5.4))
 
+    def test_force_type_raises_exception_if_unrecognized_type_provided(self):
+        with self.assertRaises(TypeError):
+            x = NumberRule(force_type="dogs")
 
+    def test_force_type(self):
+        x = NumberRule(force_type="int")
+        self.assertTrue(x.validate(5))
+        self.assertFalse(x.validate(5.4))
 
+        x = NumberRule(force_type="float")
+        self.assertFalse(x.validate(5))
+        self.assertTrue(x.validate(5.4))
 
 
 if __name__ == '__main__':
--- validator/rules/number_rule.py
+++ validator/rules/number_rule.py
@@ -1,11 +1,20 @@
 from validator.rules.base_rule import BaseRule
 
 class NumberRule(BaseRule):
-    def __init__(self, **kwargs):
+    def __init__(self, force_type=None, **kwargs):
         super().__init__(**kwargs)
+
+        if force_type and force_type not in [ "int", "float"]:
+            raise TypeError("Unrecognized number type")
+
+        self.force_type = force_type
 
     def validate(self, value):
         base_validation = super().validate(value)
 
         is_number = isinstance(value, int) or isinstance(value, float)
+
+        if self.force_type:
+            is_number = is_number and (value.__class__.__name__  == self.force_type)
+
         return is_number and base_validation
